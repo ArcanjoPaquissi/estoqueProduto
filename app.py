a6931a55698669b6126cd681edbe0918e2a4fd6f
ArcanjoPaquissi <arcanjopaquissi@gmail.com>
2025-05-21 15:18:10 -0300

    Criação e atualização de templates e ajuste do processo de autenticação e criptografia

--- app.py
+++ app.py
@@ -1,72 +1,166 @@
-from flask import Flask, render_template,request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_bcrypt import Bcrypt
+from usuario import Usuario 
+from datetime import timedelta
 import sqlite3
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "senha_muito_segura"
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60)
+
+bcrypt = Bcrypt(app)
 
 def conexao():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect("database.db")
     conn.row_factory = sqlite3.Row
     return conn
 
 @app.route("/")
 def index():
-    conn = conexao()
-    produtos = conn.execute('SELECT * FROM produtos').fetchall()
-    conn.close()
-    return render_template("index.html", produtos=produtos)
+    if 'user_id' in session:
+        conn = conexao()
+        cursor = conn.cursor()
+        produtos = cursor.execute("SELECT * FROM produtos").fetchall()
+        conn.close()
 
-@app.route("/add", methods=['GET', 'POST'])
+        return render_template('index.html', produtos=produtos)  # <-- agora passa os dados
+    else:
+        return redirect(url_for('login', next=request.url))
+
+    
+@app.route("/add", methods=["GET", "POST"])
 def add_product():
+    if 'user_id' in session:
+        if request.method == 'POST':
+            nome = request.form['nome']
+            categoria = request.form['categoria']
+            quantidade = request.form['quantidade']
+            preco = request.form['preco']
+
+            conn = conexao()
+            conn.execute(
+                "INSERT INTO produtos (nome, categoria, quantidade, preco) VALUES (?, ?, ?, ?)",
+                (nome, categoria, quantidade, preco)
+            )
+            conn.commit()
+            conn.close()
+            return redirect(url_for("index"))
+        else:
+            return render_template("add.html")  # ✅ Mostra o formulário
+    else:
+        return redirect(url_for("login"))  # ✅ Redireciona para a rota do login
+
+
+
+
+@app.route("/editar/<int:id>", methods=["GET", "POST"])
+def edit_product(id):
+    if 'user_id' not in session:
+        return redirect(url_for("login"))
+
+    conn = conexao()
+    cursor = conn.cursor()
+
     if request.method == 'POST':
         nome = request.form['nome']
         categoria = request.form['categoria']
         quantidade = request.form['quantidade']
         preco = request.form['preco']
 
-        conn = conexao()
-        conn.execute('INSERT INTO produtos (nome,categoria,quantidade,preco) values (?,?,?,?)', (nome,categoria,quantidade,preco))
+        cursor.execute("""
+            UPDATE produtos 
+            SET nome = ?, categoria = ?, quantidade = ?, preco = ?
+            WHERE id = ?
+        """, (nome, categoria, quantidade, preco, id))
+
         conn.commit()
         conn.close()
+        return redirect(url_for("index"))
+    else:
+        cursor.execute("SELECT * FROM produtos WHERE id = ?", (id,))
+        produto = cursor.fetchone()
+        conn.close()
 
-        return redirect(url_for('index'))
+        if produto:
+            return render_template("editar.html", produto=produto)
+        else:
+            return "Produto não encontrado", 404
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login = request.form['login']
+        senha = request.form['senha']
+        next_page = request.args.get('next')  # Captura a próxima página (se tiver)
+
+        conn = conexao()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE login = ?", (login,))
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            usuario = Usuario(user[0], user[1], user[2])
+            if bcrypt.check_password_hash(usuario.senha, senha):
+                session['user_id'] = usuario.id
+                session['username'] = usuario.login
+                return redirect(url_for('welcome'))
+            else:
+                return render_template('login.html', mensagem="Usuário ou Senha incorreta")
+        else:
+            return render_template('login.html', mensagem="Usuário não registrado")
+    else:
+        return render_template('login.html')
+
+@app.route('/welcome')
+def welcome():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    return render_template('welcome.html')
+
+
+@app.route("/registrar", methods=['GET', 'POST'])
+def registrar():
+    if request.method == 'POST':
+        login = request.form['login']
+        senha_plana = request.form['senha']
+        senha_hash = bcrypt.generate_password_hash(senha_plana).decode('utf-8')
+
+        try:
+            conn = conexao()
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO users (login, senha) VALUES (?, ?)", (login, senha_hash))
+            conn.commit()
+            conn.close()
+            return render_template("login.html", sucesso_mensagem="Usuário registrado com sucesso")
+        except sqlite3.IntegrityError:
+            conn.close
+            return render_template("registro.html", mensagem="Usuário já registrado")
+    else:
+        return render_template("registro.html")
     
-    return render_template('add.html')
-
-@app.route("/delete/<int:id>")
+@app.route("/deletar/<int:id>")
 def delete_product(id):
+    if 'user_id' not in session:
+        return redirect(url_for("login", next=request.url))
+
     conn = conexao()
-    conn.execute('DELETE FROM produtos where id = ?',(id,))
+    conn.execute("DELETE FROM produtos WHERE id = ?", (id,))
     conn.commit()
     conn.close()
-    return redirect(url_for('index'))
-
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit_product(id):
-    conn = conexao()
-    produto = conn.execute('SELECT * FROM produtos where id =?', (id)).fetchone()
-    if request.method == 'POST' :
-        nome = request.form['nome']
-        categoria = request.form['categoria']
-        quantidade = request.form['quantidade']
-        preco = request.form['preco']
-
-        conn.execute('update produtos SET nome = ?, categoria = ?, quantidade = ?, preco = ?, where id = ?', (nome, categoria, quantidade, preco, id))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('index'))
-    
-    conn.close()
-    return render_template('edit.html', produto=produto)
+    return redirect(url_for("index"))
 
 
+@app.route("/bemVindo")
+def bem_vindo():
+    if 'user_id' not in session:
+        return render_template("login.html", mensagem="Sessão expirada")
+    return render_template("bemVindo.html")
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
 
-if __name__ == '__main__': 
-    app.run(port=80, debug=True)
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True, port=8070)
--- init_db.py
+++ init_db.py
@@ -1,18 +1,32 @@
 import sqlite3
-
-conn = sqlite3.connect('database.db')
+ 
+conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
-
-cursor.execute('''
-CREATE TABLE produtos (
+ 
+tabela = 'produtos'  # ou 'usuarios'
+ 
+cursor.execute(f"PRAGMA table_info({tabela});")
+colunas = [col[1] for col in cursor.fetchall()]
+ 
+cursor.execute(f"SELECT * FROM {tabela};")
+registros = cursor.fetchall()
+ 
+print(f"Dados da tabela '{tabela}':")
+print(" | ".join(colunas))
+print("-" * 40)
+for registro in registros:
+    print(" | ".join(str(c) for c in registro))
+cursor.executescript(
+    '''
+    CREATE TABLE if not exists produtos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nome TEXT not null,
             categoria TEXT not null,
             quantidade INTEGER not null,
             preco REAL not null
-)
+);
 '''
 )
 conn.commit()
 conn.close()
-print("Banco de dados criado com sucesso")
+# print("Banco de dados criado com sucesso")
